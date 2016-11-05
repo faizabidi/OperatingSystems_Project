63fc312fc0d7c564b558ef8961844b69c79f81d8
Faiz Abidi <fabidi89@vt.edu>
2016-11-05 21:04:40 +0000

    Copy files to remote machine.

--- copy_files.bash
+++ copy_files.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
-	echo "No arguments provided. Give the filename to copy."
+	echo "No arguments provided. Give the filename, and path on the remote server to copy."
 	exit 1
 fi
 
 filename=$1
+path=$2
 
-CMD="scp $1"
+CMD="scp -r $1"
 
 for i in {2..10}; do
-     scp $filename 192.168.1.$i:/home/pi/spark-2.0.1-bin-hadoop2.7/conf
+     scp -r $filename 192.168.1.$i:$path
 done
     
