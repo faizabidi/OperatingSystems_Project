57e90397d5505a073548b449860de643ed7ba6fe
Faiz Abidi <fabidi89@vt.edu>
2016-11-24 23:27:26 +0000

    Minor edit.

--- copy_files.bash
+++ copy_files.bash
@@ -11,6 +11,10 @@ path=$2
 #CMD="scp -r $1"
 
 for i in {2..10}; do
-     scp -r $filename 192.168.1.$i:$path
+	if scp -r -o ConnectTimeout=5 $filename 192.168.1.$i:$path; then
+		echo "192.168.1.$i done!"
+	else
+		echo "192.168.1.$i is not reachable."
+	fi
 done
     
