6c49767a136217bd92a3bed8dc7dfeb0ea20aaea
Faiz Abidi <fabidi89@vt.edu>
2016-11-24 23:26:40 +0000

    Minor changes.

--- restart_slaves.bash
+++ restart_slaves.bash
@@ -4,6 +4,9 @@ START_CMD="/home/pi/spark-2.0.1-bin-hadoop2.7/sbin/start-slave.sh spark://192.16
 STOP_CMD="/home/pi/spark-2.0.1-bin-hadoop2.7/sbin/stop-slave.sh"
 
 for i in {2..10}; do
-	echo $STOP_CMD | ssh 192.168.1.$i bash 	
-	echo $START_CMD | ssh 192.168.1.$i bash
+	if echo $STOP_CMD | ssh -o ServerAliveCountMax=5 -o ServerAliveInterval=1 192.168.1.$i bash && echo $START_CMD | ssh -o ServerAliveCountMax=5 -o ServerAliveInterval=1 192.168.1.$i bash; then
+		echo "192.168.1.$i successfully restared."
+	else
+		echo "192.168.1.$i is not reachable."
+	fi
 done
